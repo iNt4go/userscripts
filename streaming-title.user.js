0cbee49edefdf6152bfa97d321ae1418cecc82ff
int4go <intago@Gmail.com>
2026-03-05 00:47:00 -0600

    first commit

---
+++ streaming-title.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Streaming Site Window Title
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Changes window title for streaming sites to enable Hyprland windowrules
+// @author       You
+// @match        *://*.youtube.com/*
+// @match        *://plex.*/*
+// @match        *://*.hbomax.com/*
+// @match        *://*.hbo.com/*
+// @match        *://*.primevideo.com/*
+// @match        *://*.amazon.com/gift-cards/*
+// @match        *://*.disneyplus.com/*
+// @match        *://*.netflix.com/*
+// @match        *://*.twitch.tv/*
+// @match        *://*.peacocktv.com/*
+// @match        *://peacocktv.com/*
+// @match        *://*.hulu.com/*
+// @match        *://hulu.com/*
+// @match        *://*.crunchyroll.com/*
+// @match        *://*.paramountplus.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const STREAM_SUFFIX = ' [Stream]';
+
+    function updateTitle() {
+        if (!document.title.endsWith(STREAM_SUFFIX)) {
+            document.title = document.title + STREAM_SUFFIX;
+        }
+    }
+
+    const observer = new MutationObserver(updateTitle);
+    observer.observe(document.documentElement, { childList: true, subtree: true });
+
+    window.addEventListener('load', updateTitle);
+})();
